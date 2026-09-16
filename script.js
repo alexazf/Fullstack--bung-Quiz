a22f913f39f43e7c104a0c0e682d6221944b4c5a
alexa.fink <alexa.fink@accenture.com>
2026-09-16 16:59:34 +0200

    Übungen Mittwoch

--- script.js
+++ script.js
@@ -1,3 +1,65 @@
-console.log("Hello World!");
+// console.log("Hello World!");
+
+
+
+// let number = 5; //Number
+
+// let text = "Döner"; //String
+
+// let bestanden = true; //Boolean
+
+
+// number = 42;
+// text = "Pizza";
+// bestanden = false;
+// // Variablen neuen Wert zuweisen
+
+// console.log(number);
+// console.log(text);
+// console.log(bestanden);
+// //Werte der Variablen ausgeben
+
+// let neueVariable = text;
+// console.log(neueVariable);
+// // Neue Variable erstellen und den Wert einer anderen Variable zuweisen
+// let zahl1 = 5;
+// let zahl2 = 10;
+
+// // console.log(zahl1 + zahl2); // Addition
+// // console.log(zahl1 - zahl2); // Subtraktion
+// // console.log(zahl1 * zahl2); // Multiplikation
+// // console.log(zahl1 / zahl2); // Division
+
+// // zahl1++;
+// // zahl1 = zahl1 + 1;
+// // console.log(zahl1);
+
+// zahl1 = zahl1 + zahl2;
+// zahl1 += 5;
+// console.log(zahl1);
+
+// let firstName = "Benjamin";
+// let lastName = "Mai";
+
+// let fullName = firstName + " " + lastName;
+
+// console.log(fullName);
+
+// console.log(5 == '5'); // true
+// console.log(5 === '5'); // false
+// console.log(5 != '5'); // false
+// console.log(5 <= '5'); // true
+// console.log(5 > '5'); // false
+
+// console.log("Alpha" < "Beta"); // true
+
+function functionName(zahl1, zahl2) {
+    let result = zahl1 + zahl2;
+    return result;
+}
+
+functionName(5, 5);
+console.log(functionName(5, 5));
+
 
 
